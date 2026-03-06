6b8dcedc93f572bf423c48797516492d3ed11fe8
m-azhar555 <ma0024923@gmail.com>
2026-03-06 12:27:59 +0500

    db connect

---
+++ models/Expense.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const ExpenseSchema = new mongoose.Schema({
+    employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    amount: { type: Number, required: true },
+    purpose: { type: String, required: true },
+    status: { 
+        type: String, 
+        enum: ['Pending', 'Approved', 'Rejected'], 
+        default: 'Pending' 
+    },
+    documentUrl: { type: String } // Agar koi receipt upload kare
+}, { timestamps: true });
+
+module.exports = mongoose.model('Expense', ExpenseSchema);
