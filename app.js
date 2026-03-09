0b750ce97737895ae69c0dc558252be48e11d5e0
m-azhar555 <ma0024923@gmail.com>
2026-03-09 17:53:47 +0500

    admin

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ dotenv.config();
 const express = require('express');
 const AuthRouters = require('./routes/Auth');
 const ExpenseRouters = require('./routes/Expense'); // 👈 Yeh nayi line add karein
+const AdminRouters = require('./routes/Admin');
 
 const app = express();
 const connectDB = require('./db/connect');
-
+const cors = require('cors');
+app.use(cors()); // Ye line app.use(express.json()) ke upar add karein
 app.use(express.json());
 
 app.use('/auth', AuthRouters);
 app.use('/expenses', ExpenseRouters); // 👈 Yeh nayi line add karein
+app.use('/admin', AdminRouters);
 
 const port = process.env.PORT || 5000;
 
